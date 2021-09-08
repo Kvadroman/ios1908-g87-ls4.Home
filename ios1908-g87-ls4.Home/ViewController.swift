81ff7b6ceee892c9f596952b60f81bf650f111e0
Kvadroman <kvadrostoplerman@gmail.com>
2021-09-08 18:46:03 +0300

    LS 4 Assignment #3
    Задача 2

--- ios1908-g87-ls4.Home/ViewController.swift
+++ ios1908-g87-ls4.Home/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         name(name: "Anton")
         name2(name: "Anton")
+        patronymic(patronymic: "antonovich")
     }
     //    Задача №1 1 способ
     func name (name: String) {
@@ -28,7 +29,17 @@ class ViewController: UIViewController {
     func name2 (name: String) {
         print(name.count)
     }
+    
+//        Задача №2
+    func patronymic (patronymic: String) {
+        patronymic.contains("ich") ? print("\(patronymic) contains ich") : print("\(patronymic) did not contains ich")
+        patronymic.contains("evna") ? print("\(patronymic) contains evna") : print("\(patronymic) did not contains evna")
+    }
+    
+    
+      
 }
+
 
 
 
